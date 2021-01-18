ab2bd2a60be6282ebea3f418c893175cdf2eab91
Paul Frazee <pfrazee@gmail.com>
2021-01-18 11:34:44 -0600

    Implement profiles API

--- api/profiles.js
+++ api/profiles.js
@@ -1,9 +1,27 @@
+import { userDbs } from '../db/index.js'
+import { constructUserUrl } from '../lib/strings.js'
 
 export function setup (wsServer) {
-  wsServer.register('profiles.get', async (params) => {
-    return 'todo'
+  wsServer.register('profiles.get', async ([username]) => {
+    const userDb = userDbs.get(username)
+    if (!userDb) throw new Error('User database not found')
+
+    const profileEntry = await userDb.profile.get('self')
+    if (!profileEntry) {
+      throw new Error('User profile not found')
+    }
+    profileEntry.url = constructUserUrl(username)
+    return profileEntry
   })
-  wsServer.register('profiles.put', async (params) => {
-    return 'todo'
+
+  wsServer.register('profiles.put', async ([profile], client) => {
+    if (!client?.auth) throw new Error('Must be logged in')
+    const userDb = userDbs.get(client.auth.username)
+    if (!userDb) throw new Error('User database not found')
+
+    await userDb.profile.put('self', profile)
+    
+    const url = constructUserUrl(client.auth.username)
+    return {key: client.auth.username, url}
   })
 }
---
+++ tests/profiles.js
@@ -0,0 +1,41 @@
+import test from 'ava'
+import { createServer } from './_util.js'
+
+let close
+let api
+let posts = []
+
+test.before(async () => {
+  let inst = await createServer()
+  close = inst.close
+  api = inst.api
+
+  await inst.db.createUser({
+    username: 'bobo',
+    email: 'bobo@roberts.com',
+    profile: {
+      displayName: 'Bobo Roberts'
+    }
+  })
+  await api.accounts.login({username: 'bobo', password: 'password'})
+})
+
+test.after.always(async t => {
+	await close()
+})
+
+test('basic CRUD', async t => {
+  await api.profiles.put({
+    displayName: 'Bobo Roberts',
+    description: 'Some person',
+    homepageUrl: 'http://example.com'
+  })
+
+  let profile1 = await api.profiles.get('bobo')
+  t.is(profile1.value.displayName, 'Bobo Roberts')
+  t.is(profile1.value.description, 'Some person')
+  t.is(profile1.value.homepageUrl, 'http://example.com')
+
+  await t.throwsAsync(() => api.profiles.put({}))
+  await t.throwsAsync(() => api.profiles.put({description: 'hi'}))
+})
