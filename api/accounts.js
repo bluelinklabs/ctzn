d68a44e4e3b69e49e5c25abb47dc0a7b465ac230
Paul Frazee <pfrazee@gmail.com>
2021-02-09 11:56:42 -0600

    Implement accounts.register()

--- api/accounts.js
+++ api/accounts.js
@@ -3,7 +3,21 @@ import { v4 as uuidv4 } from 'uuid'
 import { publicServerDb, privateServerDb, createUser } from '../db/index.js'
 import { constructUserUrl, constructUserId } from '../lib/strings.js'
 
+
 const registerParam = createValidator({
+  type: 'object',
+  required: ['username', 'displayName'],
+  additionalProperties: false,
+  properties: {
+    username: {type: 'string', pattern: "^([a-zA-Z][a-zA-Z0-9]{2,63})$"},
+    email: {type: 'string', format: "email"},
+    password: {type: 'string', minLength: 1},
+    displayName: {type: 'string', minLength: 1, maxLength: 64},
+    description: {type: 'string', maxLength: 256}
+  }
+})
+
+const loginParam = createValidator({
   type: 'object',
   required: ['username', 'password'],
   properties: {
@@ -51,7 +65,7 @@ export function setup (wsServer) {
   })
 
   wsServer.register('accounts.login', async (params, client) => {
-    registerParam.assert(params[0])
+    loginParam.assert(params[0])
     let {username, password} = params[0]
 
     // TODO validate credentials
@@ -63,24 +77,11 @@ export function setup (wsServer) {
     if (!user) {
       throw new Error('User not found')
     }
-
-    // create session
-    const sess = {
-      sessionId: uuidv4(),
-      username,
-      createdAt: (new Date()).toISOString()
-    }
-    await privateServerDb.accountSessions.put(sess.sessionId, sess)
-
-    client.auth = {
-      sessionId: sess.sessionId,
-      userId: constructUserId(username),
-      username,
-      dbUrl: user.value.dbUrl
-    }
+    const userId = constructUserId(username)
+    const sess = await createSession(client, {username, userId, dbUrl: user.value.dbUrl})
 
     return {
-      userId: constructUserId(username),
+      userId,
       url: constructUserUrl(username),
       dbUrl: user.value.dbUrl,
       sessionId: sess.sessionId,
@@ -95,11 +96,53 @@ export function setup (wsServer) {
     client.auth = undefined
   })
 
-  wsServer.register('accounts.register', async (params, socket_id) => {
-    return 'todo'
+  wsServer.register('accounts.register', async ([info], client) => {
+    info = info || {}
+    registerParam.assert(info)
+    
+    const citizenUser = await createUser({
+      type: 'citizen',
+      username: info.username,
+      email: info.email,
+      password: info.password,
+      profile: {
+        displayName: info.displayName,
+        description: info.description
+      }
+    })
+
+    const username = info.username
+    const userId = constructUserId(username)
+    const dbUrl = citizenUser.publicUserDb.url
+    const sess = await createSession(client, {username, userId, dbUrl})
+    return {
+      userId,
+      url: constructUserUrl(username),
+      dbUrl,
+      sessionId: sess.sessionId,
+      username
+    }
   })
 
   wsServer.register('accounts.unregister', async (params, socket_id) => {
     return 'todo'
   })
+}
+
+async function createSession (client, {username, userId, dbUrl}) {
+  const sess = {
+    sessionId: uuidv4(),
+    username,
+    createdAt: (new Date()).toISOString()
+  }
+  await privateServerDb.accountSessions.put(sess.sessionId, sess)
+
+  client.auth = {
+    sessionId: sess.sessionId,
+    userId,
+    username,
+    dbUrl
+  }
+
+  return sess
 }
