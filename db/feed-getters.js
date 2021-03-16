73914d4f2008178a8bd2ebb795fb544d2afe556e
Paul Frazee <pfrazee@gmail.com>
2021-03-16 13:29:11 -0500

    Add another debugging utility to track down the hyperbee stall issue

--- db/feed-getters.js
+++ db/feed-getters.js
@@ -8,85 +8,121 @@ import * as errors from '../lib/errors.js'
 const lexintEncoder = lexint('hex')
 
 export async function listHomeFeed (opts, auth) {
-  opts = opts && typeof opts === 'object' ? opts : {}
-  opts.lt = opts.lt && typeof opts.lt === 'string' ? opts.lt : lexintEncoder.encode(Date.now())
+  /**
+   * DEBUG
+   * We're experiencing some issues with hyperbees which cause the feed to fail
+   * to load. This 'whereIWas' race is designed to help us isolate where the
+   * failure is occurring. It should be removed eventually - we're deploying it
+   * now so we can isolate the issues on live alpha servers.
+   * - prf
+   */
+  let whereIWas = 'init'
+  let promise = (async () => {
+    opts = opts && typeof opts === 'object' ? opts : {}
+    opts.lt = opts.lt && typeof opts.lt === 'string' ? opts.lt : lexintEncoder.encode(Date.now())
 
-  if (!auth) throw new errors.SessionError()
-  const publicUserDb = publicUserDbs.get(auth.userId)
-  if (!publicUserDb) throw new errors.NotFoundError('User database not found')
+    if (!auth) throw new errors.SessionError()
+    const publicUserDb = publicUserDbs.get(auth.userId)
+    if (!publicUserDb) throw new errors.NotFoundError('User database not found')
 
-  const followEntries = await publicUserDb.follows.list()
-  followEntries.unshift({value: {subject: auth}})
-  const membershipEntries = await publicUserDb.memberships.list()
-  const sourceDbs = [
-    ...followEntries.map(f => publicUserDbs.get(f.value.subject.userId)),
-    ...membershipEntries.map(m => publicUserDbs.get(m.value.community.userId))
-  ]
+    whereIWas = 'listing follows'
+    const followEntries = await publicUserDb.follows.list()
+    followEntries.unshift({value: {subject: auth}})
+    whereIWas = 'listing memberships'
+    const membershipEntries = await publicUserDb.memberships.list()
+    whereIWas = 'getting databases'
+    const sourceDbs = [
+      ...followEntries.map(f => publicUserDbs.get(f.value.subject.userId)),
+      ...membershipEntries.map(m => publicUserDbs.get(m.value.community.userId))
+    ]
+    
+    whereIWas = 'initializing cursors'
+    const cursors = sourceDbs.map(db => {
+      if (!db) return undefined
+      if (db.dbType === 'ctzn.network/public-citizen-db') {
+        return db.posts.cursorRead({lt: opts?.lt, reverse: true})
+      } else if (db.dbType === 'ctzn.network/public-community-db') {
+        return db.feedIdx.cursorRead({lt: opts?.lt, reverse: true})
+      }
+    })
 
-  const cursors = sourceDbs.map(db => {
-    if (!db) return undefined
-    if (db.dbType === 'ctzn.network/public-citizen-db') {
-      return db.posts.cursorRead({lt: opts?.lt, reverse: true})
-    } else if (db.dbType === 'ctzn.network/public-community-db') {
-      return db.feedIdx.cursorRead({lt: opts?.lt, reverse: true})
-    }
-  })
-
-  const postEntries = []
-  const authorsCache = {}
-  const limit = Math.min(opts?.limit || 100, 100)
-  const mergedCursor = mergeCursors(cursors)
-  for await (let [db, entry] of mergedCursor) {
-    if (db.dbType === 'ctzn.network/public-community-db') {
-      const {origin, key} = parseEntryUrl(entry.value.item.dbUrl)
+    async function* mergeCursors (cursors) {
+      let cursorResults = []
       
-      const userId = await fetchUserId(origin)
-      const publicUserDb = publicUserDbs.get(userId)
-      if (!publicUserDb) continue
-
-      entry = await publicUserDb.posts.get(key)
-      if (!entry) continue
-      db = publicUserDb
-    } else {
-      if (entry.value.community) {
-        continue // filter out community posts by followed users
-      }
-    }
-    entry.url = constructEntryUrl(db.url, 'ctzn.network/post', entry.key)
-    entry.author = await fetchAuthor(db.userId, authorsCache)
-    entry.reactions = (await fetchReactions(entry, auth?.userId)).reactions
-    entry.replyCount = await fetchReplyCount(entry, auth?.userId)
-    postEntries.push(entry)
-    if (postEntries.length >= limit) {
-      break
-    }
-  }
-
-  return postEntries
-}
-
-async function* mergeCursors (cursors) {
-  let cursorResults = []
-  
-  while (true) {
-    let bestI = -1
-    for (let i = 0; i < cursors.length; i++) {
-      if (!cursors[i]) continue
-      if (!cursorResults[i]?.length) {
-        cursorResults[i] = await cursors[i].next(10)
-      }
-      if (cursorResults[i]?.length) {
-        if (bestI === -1) {
-          bestI = i
-        } else {
-          if (cursorResults[i][0].key.localeCompare(cursorResults[bestI][0].key) === 1) {
-            bestI = i
+      while (true) {
+        let bestI = -1
+        for (let i = 0; i < cursors.length; i++) {
+          if (!cursors[i]) continue
+          if (!cursorResults[i]?.length) {
+            whereIWas = `calling cursor.next for ${cursors[i].db._ident}`
+            cursorResults[i] = await cursors[i].next(10)
+          }
+          if (cursorResults[i]?.length) {
+            if (bestI === -1) {
+              bestI = i
+            } else {
+              if (cursorResults[i][0].key.localeCompare(cursorResults[bestI][0].key) === 1) {
+                bestI = i
+              }
+            }
           }
         }
+    
+        if (bestI === -1) return // out of results
+        yield [cursors[bestI].db, cursorResults[bestI].shift()]
       }
     }
 
-    if (bestI === -1) return // out of results
-    yield [cursors[bestI].db, cursorResults[bestI].shift()]
-  }
+    const postEntries = []
+    const authorsCache = {}
+    const limit = Math.min(opts?.limit || 100, 100)
+    const mergedCursor = mergeCursors(cursors)
+    whereIWas = 'starting the for await'
+    for await (let [db, entry] of mergedCursor) {
+      if (db.dbType === 'ctzn.network/public-community-db') {
+        const {origin, key} = parseEntryUrl(entry.value.item.dbUrl)
+        
+        whereIWas = `fetching the userId for ${origin}`
+        const userId = await fetchUserId(origin)
+        const publicUserDb = publicUserDbs.get(userId)
+        if (!publicUserDb) continue
+
+        whereIWas = `getting the post ${key}`
+        entry = await publicUserDb.posts.get(key)
+        if (!entry) continue
+        db = publicUserDb
+      } else {
+        if (entry.value.community) {
+          continue // filter out community posts by followed users
+        }
+      }
+      entry.url = constructEntryUrl(db.url, 'ctzn.network/post', entry.key)
+      whereIWas = `fetching the author ${db.userId}`
+      entry.author = await fetchAuthor(db.userId, authorsCache)
+      whereIWas = `fetching the reactions for ${entry.url}`
+      entry.reactions = (await fetchReactions(entry, auth?.userId)).reactions
+      whereIWas = `fetching the reply count for ${entry.url}`
+      entry.replyCount = await fetchReplyCount(entry, auth?.userId)
+      postEntries.push(entry)
+      if (postEntries.length >= limit) {
+        break
+      }
+    }
+    whereIWas = 'done'
+
+    return postEntries
+  })()
+
+  let to
+  const toPromise = new Promise((resolve) => {
+    to = setTimeout(() => {
+      console.log('HOME FEED TIMED OUT at:', whereIWas)
+      resolve([])
+    }, 60e3)
+    to.unref()
+  })
+  const clearTO = () => clearTimeout(to)
+  const res = Promise.race([promise, toPromise])
+  res.then(clearTO, clearTO)
+  return res
 }
