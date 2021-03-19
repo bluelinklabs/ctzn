4d7a1ee7454218e07be07ca6210c3ba03581f3a0
Paul Frazee <pfrazee@gmail.com>
2021-03-19 15:17:01 -0500

    Apply timeout on put-avatar method

--- db/dbmethods/put-avatar.js
+++ db/dbmethods/put-avatar.js
@@ -2,16 +2,17 @@ import { assertUserPermission } from './_util.js'
 import { blobGet } from '../util.js'
 import * as errors from '../../lib/errors.js'
 import { Config } from '../../lib/config.js'
+import { timeoutRace } from '../../lib/functions.js'
 import bytes from 'bytes'
 
 export default async function (db, caller, args) {
   await assertUserPermission(db, caller.userId, 'ctzn.network/perm-community-edit-profile')
-  const avatar = await blobGet(args.blobSource.userId, args.blobName)
+  const avatar = await timeoutRace(30e3, undefined, blobGet(args.blobSource.userId, args.blobName))
   if (!avatar) {
     throw new Error('Failed to read blob from source database')
   }
   if (avatar.length > (Config.getActiveConfig()?.avatarSizeLimit || 0)) {
-    throw new errors.ValidationError(`Your avatar image is too big! It must be smaller than ${bytes(config.avatarSizeLimit)}.`)
+    throw new errors.ValidationError(`Your avatar image is too big! It must be smaller than ${bytes(Config.getActiveConfig().avatarSizeLimit)}.`)
   }
   await db.blobs.put('avatar', avatar)
 }
